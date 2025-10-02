f42169013a16c6f2af64b8d90a3de65ad3fe51a9
Zyphon12342 <shivansh12342@gmail.com>
2025-10-02 20:02:43 +0530

    Added hittable_list (World using shared_ptr) and utility header for convinience

--- main.cpp
+++ main.cpp
@@ -1,61 +1,44 @@
-#include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "rtweekend.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 
-#include <iostream>
 
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc,oc) - (radius*radius);
-    auto discriminant = b*b - 4*(a*c);
-    
-    /*
-    3 cases: 
-        D > 0, Two intersecting points with spehere(entry && exit)
-        D == 0, One intersection tangential
-        D < 0, No solution / intersection
-    */
-    if (discriminant < 0) {
-        return -1.0;
-    } else{
-        return (-b -std::sqrt(discriminant)) / (2.0 * a);
-    }
-}
 
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    // Rendering sphere with hard coded radius
-    
-    if(t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if(world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
 
-    // using a lerp(linear interpolation) b/w 2 values (blue and white) to linearly blend to values
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    // (1-a) * startValue + a* endValue
-    // Start = White, End = blue
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+
 }
 
 
 int main() 
 
 {
-    // Image 
+    // Image
+
     auto aspect_ratio = 16.0 / 9.0; 
-    int image_width = 1000;
+    int image_width = 1920;
 
     // Calculating image height based of aspect ratio with min val 1
     int image_height = int(image_width / aspect_ratio); 
     image_height = (image_height < 1 ? 1 : image_height);
 
+
+    // World
+    
+    hittable_list world;
+    
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
 
     // Camera
@@ -96,7 +79,7 @@ int main()
             
             ray r(camera_center, ray_direction);
             
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
